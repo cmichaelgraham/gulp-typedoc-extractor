52b94bd2d4bb630f421e1240697c1b5e06bcfcc1
Mike Graham <mgraham@technicalmedia.com>
2015-08-08 09:00:01 -0600

    basics are working

--- index.js
+++ index.js
@@ -1,77 +1,33 @@
 'use strict';
-
+var gutil = require('gulp-util');
+var path = require('path');
+var nconf = require('nconf');
+var File = require('vinyl');
 var Transform = require('readable-stream/transform');
-var rs = require('replacestream');
-var istextorbinary = require('istextorbinary');
 
-module.exports = function(search, replacement, options) {
+module.exports = function(moduleName) {
   return new Transform({
     objectMode: true,
     transform: function(file, enc, callback) {
       if (file.isNull()) {
         return callback(null, file);
       }
-
-      function doReplace() {
-        if (file.isStream()) {
-          file.contents = file.contents.pipe(rs(search, replacement));
-          return callback(null, file);
-        }
-
-        if (file.isBuffer()) {
-          if (search instanceof RegExp) {
-            file.contents = new Buffer(String(file.contents).replace(search, replacement));
-          }
-          else {
-            var chunks = String(file.contents).split(search);
-
-            var result;
-            if (typeof replacement === 'function') {
-              // Start with the first chunk already in the result
-              // Replacements will be added thereafter
-              // This is done to avoid checking the value of i in the loop
-              result = [ chunks[0] ];
-
-              // The replacement function should be called once for each match
-              for (var i = 1; i < chunks.length; i++) {
-                // Add the replacement value
-                result.push(replacement(search));
-
-                // Add the next chunk
-                result.push(chunks[i]);
-              }
-
-              result = result.join('');
-            }
-            else {
-              result = chunks.join(replacement);
-            }
-
-            file.contents = new Buffer(result);
-          }
-          return callback(null, file);
-        }
-
-        callback(null, file);
+      else if ( file.isStream() ) {
+        this.emit('error', new gutil.PluginError('gulp-typedoc-extractor', 'Streaming not supported.'));
+        return callback();
       }
 
-      if (options && options.skipBinary) {
-        istextorbinary.isText(file.path, file.contents, function(err, result) {
-          if (err) {
-            return callback(err, file);
-          }
+      try {
+        var contents = file.contents.toString("utf-8");
+        var obj = JSON.parse(contents);
+        var outObj = obj.children[0].children[0];
+        file.contents = new Buffer(JSON.stringify(outObj));
+        return callback(null, file);
 
-          if (!result) {
-            callback(null, file);
-          } else {
-            doReplace();
-          }
-        });
-
-        return;
+      } catch (e) {
+        this.emit('error', new gutil.PluginError('gulp-typedoc-extractor', 'Error:'));
+        callback(e);
       }
-
-      doReplace();
-    }
-  });
+  }
+});
 };
